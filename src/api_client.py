4f0d78e3621cb0747e1318ac132dae4b6852bb7c
Moritz Renkin <moritz@renkin.cc>
2023-07-02 16:40:19 +0200

    added file upload

--- src/api_client.py
+++ src/api_client.py
@@ -1,8 +1,10 @@
 import requests
 import os
 import json
+import logging
+from pathlib import Path
 
-
+logger = logging.getLogger("ApiClient")
 prj_dir = os.path.dirname(__file__)
 token_file_path = os.path.join(prj_dir, "api-access-token.txt")
 
@@ -14,20 +16,63 @@ class ApiClient:
             self.__token: str = token_file.read()
             self.__token_suffix: str = f"?access_token={self.__token}"
 
-    def build_url(self, rel_path:str):
+    def _build_url(self, rel_path:str) -> str:
         return self.base_url + rel_path.strip("/") + self.__token_suffix
 
     def get_all_records(self) -> dict:
-        url = self.build_url("records/")
+        url = self._build_url("records/")
         r = requests.get(url)
         return json.loads(r.text)
 
-    def create_draft_record(self, draft_json: dict) -> dict:
+    def create_draft(self, draft_json: dict) -> str:
         """
-        TODO Documentation
-        :param draft_json:
+        Create a draft record without publishing.
+
+        :param draft_json: Message Body according to InvenioRDM documentation.
+        :return: ID of the draft record
+        """
+        logger.debug("Creating draft record.")
+        url = self._build_url("records/")
+        response = requests.post(url, json=draft_json)
+        logger.debug(f"API Response body: {response.text}")
+        record_id = json.loads(response.text)["id"]
+        return record_id
+
+    def upload_draft_files(self, record_id: str, file_paths: list[str]):
+        """
+        TODO docu
+        :param record_id: ID of the draft record
+        :param file_paths:
         :return:
         """
-        url = self.build_url("records/")
-        r = requests.post(url, json=draft_json)
-        return json.loads(r.text)
+
+        # Create files on server
+        create_url = self._build_url(f"records/{record_id}/draft/files")
+
+        paths: list[Path] = [Path(path) for path in file_paths]
+        file_names: list[str] = [path.name for path in paths]
+
+        create_body = [{"key": filename} for filename in file_names]
+        create_response = requests.post(create_url, json=create_body)
+
+        # Upload binary data to files
+        for path in paths:
+            filename = path.name
+            upload_url = self._build_url(f"records/{record_id}/draft/files/{filename}/content")
+            upload_header = {"Content-Type": "application/octet-stream"}
+            file_content = path.read_bytes()
+            upload_response = requests.put(upload_url, headers=upload_header, data=file_content)
+            pass
+
+
+
+    def publish_draft(self, record_id: str):
+        """
+        TODO docu
+        :param record_id: ID of the draft record
+        :return:
+        """
+        logger.debug("Publishing draft record.")
+        url = self._build_url(f"records/{record_id}/draft/actions/publish")
+        response = requests.post(url)
+        print(response)
--- src/main.py
+++ src/main.py
@@ -1,51 +1,52 @@
 from api_client import ApiClient
 
+test_record = {
+  "access": {
+    "record": "public",
+    "files": "public"
+  },
+  "files": {
+    "enabled": True
+  },
+  "metadata": {
+    "creators": [
+      {
+        "person_or_org": {
+          "family_name": "Brown",
+          "given_name": "Troy",
+          "type": "personal"
+        }
+      },
+      {
+        "person_or_org": {
+          "family_name": "Collins",
+          "given_name": "Thomas",
+          "identifiers": [
+            {"scheme": "orcid", "identifier": "0000-0002-1825-0097"}
+          ],
+          "name": "Collins, Thomas",
+          "type": "personal"
+        },
+        "affiliations": [
+          {
+            "id": "01ggx4157",
+            "name": "Entity One"
+          }
+        ]
+      }
+    ],
+    "publication_date": "2020-06-01",
+    "resource_type": { "id": "image" },
+    "title": "A Romans story",
+  }
+}
+
 def main():
     client = ApiClient()
-    records = client.get_all_records()
-    print(records)
-
-    test_record = {
-          "access": {
-            "record": "public",
-            "files": "public"
-          },
-          "files": {
-            "enabled": True
-          },
-          "metadata": {
-            "creators": [
-              {
-                "person_or_org": {
-                  "family_name": "Brown",
-                  "given_name": "Troy",
-                  "type": "personal"
-                }
-              },
-              {
-                "person_or_org": {
-                  "family_name": "Collins",
-                  "given_name": "Thomas",
-                  "identifiers": [
-                    {"scheme": "orcid", "identifier": "0000-0002-1825-0097"}
-                  ],
-                  "name": "Collins, Thomas",
-                  "type": "personal"
-                },
-                "affiliations": [
-                  {
-                    "id": "01ggx4157",
-                    "name": "Entity One"
-                  }
-                ]
-              }
-            ],
-            "publication_date": "2020-06-01",
-            "title": "A Romans story",
-          }
-        }
-
-    print(client.create_draft_record(test_record))
+    #records = client.get_all_records()
+    record_id = client.create_draft(test_record)
+    client.upload_draft_files(record_id=record_id, file_paths=["src/testfile.txt"])
+    client.publish_draft(record_id)
 
 if __name__ == '__main__':
     main()
