1e2d7d2fabc057276899def31c557861141cebd4
Moritz Renkin <moritz@renkin.cc>
2023-07-03 19:51:37 +0200

    test rocrates

--- tuw-rocrate-automation/rocrate_datacite_conversion.py
+++ tuw-rocrate-automation/rocrate_datacite_conversion.py
@@ -1,8 +1,18 @@
 from rocrate.rocrate import ROCrate
 from pathlib import Path
 
+TEMPLATE_DATACITE_RECORD = {
+    "access": {
+        "record": "public",
+        "files": "public"
+      },
+    "files": {
+        "enabled": True
+      },
+    "metadata": {}
+}
 
-def get_datacite_record_from_rocrate(rocrate_path: str | Path) -> dict:
+def generate_datacite_record(rocrate_path: str | Path) -> dict:
     """
     TODO docu
     :param rocrate_path:
@@ -10,10 +20,20 @@ def get_datacite_record_from_rocrate(rocrate_path: str | Path) -> dict:
     """
     rocrate_path = Path(rocrate_path)
     crate = ROCrate(rocrate_path)
-    pass
+
+    record = TEMPLATE_DATACITE_RECORD.copy()
+    record_metadata = record['metadata']
+    record_metadata['title'] = crate.name
+    record_metadata['description'] = crate.description
+
+    creator_emails = {creator for e in crate.get_entities() if e.get("creator") for creator in e.get("creator")}
+    # TODO
+
+
+    return record
 
 
 if __name__ == '__main__':
     file_path = Path(__file__).parent.resolve()
-    rocrate_path = file_path / '../test/sample-rocrates/b97348a7-991f-46cf-9834-cf602bacf800.zip'
-    get_datacite_record_from_rocrate(rocrate_path)
+    rocrate_path = file_path / '../test/sample-rocrates/b97348a7-991f-46cf-9834-cf602bacf800'
+    generate_datacite_record(rocrate_path)
